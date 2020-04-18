a4b46efa6d7f3cf3131fd2d1c54f37547fc2ecc9
venkatasykam <venkatasykam@yahoo.com>
2020-04-18 06:14:17 +0000

    Create Sample.groovy

---
+++ Day6/Sample.groovy
@@ -0,0 +1,110 @@
+/*
+println 'Hii.. my first groovy script'
+println "Hii.. my first groovy script"
+println("Hii.. my first groovy script")
+println('Hii.. my first groovy script')
+println('-------------------------')
+System.out.println("Printing using Sys out")
+
+println "From print command/method line 1 --- "
+print "From print command/method line 2"
+println('-------------------------')
+
+// defining variables
+ multiline 
+comment
+
+
+def i = 10
+
+def str = "Simple program"
+
+def bln = true
+
+println i
+
+println str
+
+println bln
+
+
+int j = 102
+
+String str2 = "Simple program2"
+
+boolean bln2 = false
+
+println "the value of J is : " + j
+
+println "the value of str2 is : " + str2
+
+println "the value of bln2 is : " + bln2
+
+
+
+println "printing out side of the multiline comment"
+*/
+
+def a = 100
+
+int b = 200
+
+int c = a + b
+
+println "Sum of a & b is C: " + c
+
+def a = 5546
+
+int b = 989
+
+int c = a + b
+
+println "Sum of a & b is C: " + c
+
+def a = 5546
+
+int b = 989
+
+int c = a + b
+
+println "Sum of a & b is C: " + c
+
+
+
+
+
+
+
+
+
+
+
+def addition(int a,int b){
+	println "Sub of a & b is: " + (a-b)
+}
+
+def addition(int a,String b){
+	println "Value of a is: " + a
+	println "Value of b is: " + b
+}
+
+def addition(int a){
+	println "Value of a is: " + a
+}
+
+
+addition(345,345)
+
+addition(a,b)
+
+addition(34,43)
+
+addition(34,"Phani")
+
+addition(34)
+
+
+
+
+
+
